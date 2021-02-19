4564091c7195f13a6fc88899e7d7f3dab761101b
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-02-19 13:06:01 +0100

    Configure job

--- util/peregrine.sh
+++ util/peregrine.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
-#SBATCH --time=00:30:00
-#SBATCH --mem=3000
+#SBATCH --time=24:00:00
+#SBATCH --mem=50000
 #SBATCH --nodes=1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:v100:1
 #SBATCH --job-name=dl-sign-language
 #SBATCH --output=logs/slurm-%A_%a.out
+#SBATCH --array=1,5,10
 # → do make sure /logs directory exists!
 
 module load Python/3.8.2-GCCcore-9.3.0
 source venv/bin/activate
-pip3 install -r requirements.txt
-python3 src/train_model_test.py
+pip install -r requirements.txt
+python src/train_model.py \
+    --path $(sh util/data_dir.sh)/data.csv \
+    --name ${SLURM_ARRAY_JOB_ID} \
+    --epochs ${SLURM_ARRAY_TASK_ID}
