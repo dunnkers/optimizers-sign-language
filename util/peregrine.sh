62364d6fbe49bad01334765b1b10f4d04aa80253
J.G.S.Overschie@student.rug.nl <s2995697@peregrine.hpc.rug.nl>
2021-02-23 17:11:45 +0100

    SLURM dev config pg-gpu node

--- util/peregrine.sh
+++ util/peregrine.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#SBATCH --time=24:00:00
-#SBATCH --mem=128000
+#SBATCH --time=02:00:00
+#SBATCH --mem=100000
 #SBATCH --nodes=1
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:v100:1
+#SBATCH --partition=gpushort
+#SBATCH --gres=gpu:1
 #SBATCH --job-name=dl-sign-language
 #SBATCH --output=logs/slurm-%A_%a.out
-#SBATCH --array=0-7
+#SBATCH --array=0-8
 # → do make sure /logs directory exists!
 
 module load TensorFlow
-source venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt --user
 python src/train_model.py \
     --path /data/${USER}/data.csv \
     --name ${SLURM_ARRAY_JOB_ID}_${SLURM_ARRAY_TASK_ID} \
-    --optimizer ${SLURM_ARRAY_TASK_ID}
+    --optimizer ${SLURM_ARRAY_TASK_ID} \
+    --epochs 200 \
+    --weights imagenet
