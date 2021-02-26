5f27ef5c21a52a590fd0c626ec2636c81bbb750b
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-02-26 12:05:56 +0100

    Updates

---
+++ precache-manifest.95b30bf91419d91e783d59bad8409e7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c1a5c6fe32d8c7cb6abd0d7ddad523a",
+    "url": "/optimizers-sign-language/index.html"
+  },
+  {
+    "revision": "7ba38d598270b5871655",
+    "url": "/optimizers-sign-language/static/css/2.73624963.chunk.css"
+  },
+  {
+    "revision": "60e7284a45c669eb999e",
+    "url": "/optimizers-sign-language/static/css/main.4383de78.chunk.css"
+  },
+  {
+    "revision": "7ba38d598270b5871655",
+    "url": "/optimizers-sign-language/static/js/2.800966e3.chunk.js"
+  },
+  {
+    "revision": "578d08a808a50d43c871c4bbfe42d44b",
+    "url": "/optimizers-sign-language/static/js/2.800966e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60e7284a45c669eb999e",
+    "url": "/optimizers-sign-language/static/js/main.6a329ea0.chunk.js"
+  },
+  {
+    "revision": "17f83bd4c3947824db8d",
+    "url": "/optimizers-sign-language/static/js/runtime-main.9f3d028b.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/optimizers-sign-language/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
